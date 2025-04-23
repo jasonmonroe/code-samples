b8fd8ab251b1c456d445bac4a74fd23278e71cee
Jason Monroe <jwmonroe1@gmail.com>
2025-04-23 17:05:19 -0500

    cleanup

--- py/ca_paycheck.py
+++ py/ca_paycheck.py
@@ -19,7 +19,6 @@ import lorem
 import random
 
 # Constants
-
 # Threshold values for hours worked
 MIN_HOURS_PER_DAY = 0
 MAX_HOURS_PER_DAY = 24
@@ -60,91 +59,10 @@ def show_banner():
     print(" CALIFORNIA PAY CALCULATOR ")
     print("=" * 27)
 
-"""
-def count_hours():
-    standard_hours = 0
-    overtime_hours = 0
-    double_time_hours = 0
-
-    # Day hours
-    if hours_in_day > STANDARD_HOURS_PER_DAY:
-        standard_hours = STANDARD_HOURS_PER_DAY
-        overtime_hours = hours_in_day - STANDARD_HOURS_PER_DAY
-        double_time_hours = hours_in_day - DOUBLE_TIME_HOURS_PER_DAY
-
-    # Week hours
-    if hours_in_week > STANDARD_HOURS_PER_WK:
-        standard_hours = hours_in_week - hours_in_day
-        overtime_hours = hours_in_week - STANDARD_HOURS_PER_WK
-        #double_time_hours = hours_in_week - DOUBLE_TIME_HOURS_PER_DAY
-
-    return {"standard_hours": standard_hours, "overtime_hours": overtime_hours, "double_time_hours": double_time_hours}
-"""
-
-"""
-def count_day_hours():
-    standard_hours = 0
-    overtime_hours = 0
-    double_time_hours = 0
-
-    if hours_in_day > STANDARD_HOURS_PER_DAY:
-        standard_hours = STANDARD_HOURS_PER_DAY
-        overtime_hours = hours_in_day - STANDARD_HOURS_PER_DAY
-        double_time_hours = hours_in_day - DOUBLE_TIME_HOURS_PER_DAY
-
-    return {"standard_hours": standard_hours, "overtime_hours": overtime_hours, "double_time_hours": double_time_hours}
-
-def count_week_hours():
-    standard_hours = 0
-    overtime_hours = 0
-    double_time_hours = 0
-    
-    # Every hours
-    if hours_in_week > STANDARD_HOURS_PER_WK:
-        #standard_hours = STANDARD_HOURS_PER_WK
-        overtime_hours = hours_in_week - STANDARD_HOURS_PER_WK
-        double_time_hours = hours_in_week - DOUBLE_TIME_HOURS_PER_DAY
-    
-    pass
-
-def count_overtime_hours():
-
-def count_double_time_hours():
-    pass
-
-def calc_standard_day_rate():
-    print("Calculating standard...")
-    standard_hours = hours_in_day
-    if hours_in_day > STANDARD_HOURS_PER_DAY:
-        standard_hours = STANDARD_HOURS_PER_DAY
-    print('returning:', standard_hours * STANDARD_RATE * pay_rate)
-    return standard_hours * STANDARD_RATE * pay_rate
-
-def calc_overtime_day_rate():
-    print("Calculating overtime...")
-    overtime_hours = hours_in_day - STANDARD_HOURS_PER_DAY
-
-    # 1.5x4x10
-    print('Overtime hours:', overtime_hours)
-    print('pay rate:', pay_rate)
-    print('Overtime rate:', OVERTIME_RATE)
-    print('hours_in_day', hours_in_day)
-    print('returning:', overtime_hours * OVERTIME_RATE * pay_rate)
-    return overtime_hours * OVERTIME_RATE * pay_rate
-
-def calc_double_time_day_rate():
-    print("Calculating double time...")
-    double_time_hours = hours_in_day - OVERTIME_HOURS_PER_DAY
-    return double_time_hours * DOUBLE_TIME_RATE * pay_rate
-"""
-
 # Create a random company name.
 def company_name():
     word_cnt = random.randint(1, 3)
-    #print(lorem.text())
-    #words = lorem.get_sentence().split()[:word_cnt]
     words = lorem.text().split()
-    #return "Test Company, Inc."
     return ' '.join(words[:word_cnt]) + ", Inc."
 
 # 2025 Federal Tax Rates
@@ -249,7 +167,6 @@ def calc_deductions(gross: float) -> dict:
         "net": net
     }
 
-
 # Calculate federal and state taxes.
 def calc_tax(gross: float, rates: list) -> float:
     projected_gross_yr = gross * WEEKS_IN_YEAR
@@ -258,136 +175,28 @@ def calc_tax(gross: float, rates: list) -> float:
     prev_limit = None
 
     for rate, limit in rates:
-        #gross_rem = 0.0
-
         if projected_gross_yr >= limit:
-            print(f"\t Rate: {rate}, Limit: {limit}")
-            print("\tPrev limit:", prev_limit)
-            #gross_rem = gross - limit
-            #print("\tGross left:", gross_rem)
             if prev_limit is None:
                 prev_limit = limit
                 amount += limit * rate
             else:
-                print("\tLINE 318!! Prev limit is not None:", prev_limit)
-                print(f"\tLine 319 limit * rate {(limit - prev_limit) * rate}")
                 amount += (limit - prev_limit) * rate
-                print("\tlimit diff:", limit - prev_limit)
-                #print("\tAmount:", amount)
-                #amount += limit * rate
         else:
-            print("\t< then limit get the remaining amount")
             # Calculate the remaining amount then break loop.
             gross_rem = projected_gross_yr - prev_limit
-            print("\tGross Rem:", gross_rem)
-            print("\tNext rate amount", gross_rem * rate)
             amount += gross_rem * rate
-            print(f"\t({rate}%) Remaining Tax amount:", amount)
             break
 
-
-    print(f"\tFinal Tax amount:", amount)
-    # Convert back to weekly amount
     amount /= WEEKS_IN_YEAR
     return amount
 
-
-
 # Assume the weekly amount is the annual amount to calculate the federal tax.
 def calc_federal_tax(gross: float) -> float:
-    print("Calculating federal tax...")
     return calc_tax(gross, get_federal_tax_rates())
 
 # Calculate the state tax amount.
 def calc_state_tax(gross: float) -> float:
-    print("Calculating state tax...")
     return calc_tax(gross, get_state_tax_rates())
-
-
-"""
-def calc_federal_tax2(gross) -> float:
-    projected_gross_yr = gross * 52
-    print("\tProjected gross income for the year:", projected_gross_yr)
-    FED_RATES = [
-        (0.10, 11925),
-        (0.12, 48475),
-        (0.22, 103350),
-        (0.24, 197300),
-        (0.32, 250525),
-        (0.35, 626350),
-        (0.37, float("inf"))
-    ]
-
-    # Calculate the federal tax amount based on the gross income
-    amount = 0.0
-    #gross_rem = gross
-    prev_limit = None
-    for rate, limit in FED_RATES:
-        print("\tLimit:", limit)
-        print("\tRate:", rate)
-        if projected_gross_yr >= limit:
-            prev_limit = limit
-            print("\tPrev limit:", prev_limit)
-
-
-            #gross_rem = gross - limit
-            #print("\tGross left:", gross_rem)
-            if prev_limit is None:
-                amount += limit * rate
-            else:
-                amount += (limit - prev_limit) * rate
-                print("\tLimit - Prev limit:", limit - prev_limit)
-                print("\tAmount:", amount)
-
-            amount += limit * rate
-
-
-            print(f"\t{rate} Tax amount:", amount)
-            
-    print("Final Federal tax amount:", amount)
-
-    return amount
-    
-# Calculate the state tax amount.
-def calc_state_tax2(gross) -> float:
-    projected_gross_yr = gross * 52
-    print("\tProjected gross income for the year:", projected_gross_yr)
-    gross_rem = 0
-    STATE_RATES = [
-        (0.01, 10099),
-        (0.02, 23942),
-        (0.04, 37788),
-        (0.06, 52455),
-        (0.08, 66295),
-        (0.093, 338639),
-        (0.103, 406364),
-        (0.113, 677275),
-        (0.123, 1000000),
-        (0.133, float("inf"))
-    ]
-
-    amount = 0.0
-    prev_limit = None
-    for rate, limit in STATE_RATES:
-        print("\tLimit:", limit)
-        print("\tRate:", rate)
-        if projected_gross_yr >= limit:
-            prev_limit = limit
-            print("\tPrev limit:", prev_limit)
-
-            if prev_limit is None:
-                amount += limit * rate
-            else:
-                amount += (limit - prev_limit) * rate
-                print("\tLimit - Prev limit:", limit - prev_limit)
-                print("\tAmount:", amount)
-
-            amount += limit * rate
-
-    print("Final State tax amount:", amount)
-
-    return amount
-"""
 
 # Calculate the FICA tax amount.
 def calc_fica_tax(gross: float) -> dict:
@@ -408,7 +217,7 @@ def show_paycheck():
     net_pay = deductions["net"]
     effective_rate = (1 - (net_pay / gross_pay))
 
-
+    print("\n")
     print("=" * 28)
     print(" CA Weekly Paycheck Summary")
     print(f" {company_name()}")
@@ -419,15 +228,16 @@ def show_paycheck():
     print(f"Gross Pay: ${gross_pay:.2f}\n")
 
     print("Deductions")
+    print(f"Effective Rate: {effective_rate:.2%}")
     print(f"Federal Tax: -${federal:.2f}")
     print(f"State Tax: -${state:.2f}")
     print(f"Disability Tax: -${disability:.2f}")
+
     print("\nFICA")
     print(f"SS Tax: ${ss:.2f}")
     print(f"Medicare Tax: ${medicare:.2f}\n")
 
-    print(f"Net Pay: ${net_pay:.2f}")
-    print(f"Effective Rate: {effective_rate:.2%}\n")
+    print(f"Net Pay: ${net_pay:.2f}\n")
     
     print("-" * 67)
     print(" Day\t| Hours Worked || Standard | Overtime | Double Time || Pay")
@@ -441,49 +251,9 @@ def show_paycheck():
         day_double_time_hours = day_hours["hours"]["double_time_hours"]
         day_gross_pay = day_hours["pay"]
 
-        #print(f"{day}: {hours_worked}h worked | {standard}h standard, {overtime}h OT, {double}h DT | Pay: ${pay}")
         print(f"{day_str} | {hours_worked}\t\t|| {day_standard_hours}\t\t| {day_overtime_hours}\t| {day_double_time_hours}\t|| ${day_gross_pay:.2f}")
 
-
-
-        #print(f"{day_hours["day"]} | {day_hours["hours_worked"]} | {day_hours["hours"]["standard_hours"]} | {day_hours["hours"]["overtime_hours"]} | {day_hours["hours"]["double_time_hours"]} | ${day_hours["pay"]:.2f}")
-
-        """
-        for day, hours_worked in day_hours.items():
-            if day == "hours" or day == "pay":
-                continue  # skip non-day keys
-
-            hours = day_hours["hours"]
-            pay = day_hours["pay"]
-
-            print(f"{day}\t| {hours['standard_hours']} | {hours['overtime_hours']} | {hours['double_time_hours']} | Pay: ${pay}")
-
-        """
-    """      
-    for day_hours in paycheck:
-        print(day_hours)
-        day = day_hours["day"]
-        hours_worked = day_hours["hours"]
-        hours = day_hours["hours"]
-        standard = hours["standard_hours"]
-        overtime = hours["overtime_hours"]
-        double_time = hours["double_time_hours"]
-        pay = day_hours["pay"]
-
-        print(f"{day} | {hours_worked} | {standard} | {overtime} | {double_time} | ${pay:.2f}")
-
-        #for value in day_hours:
-            #print(f"{key} | {value} | {value['hours']['standard_hours']} | {value['hours']['overtime_hours']} | {value['hours']['double_time_hours']} | ${value['pay']:.2f}")
-            #print(f"{day} | {hours_worked} |   | ${pay:.2f}")
-
-            #print(f"{day} | {hours_worked} | {hours['standard_hours']} | {hours['overtime_hours']} | {hours['double_time_hours']} | ${pay:.2f}")
-        """
-
-
     print("\n")
-
-
-
 
 # Beginning of the program
 show_banner()
@@ -520,55 +290,10 @@ for day in WEEKDAYS:
     if hours_in_week > STANDARD_HOURS_PER_WK:
         standard_hours = hours_in_week - hours_in_day
         overtime_hours = hours_in_week - STANDARD_HOURS_PER_WK
-        #double_time_hours = hours_in_week - DOUBLE_TIME_HOURS_PER_DAY
 
     # Calculate double time for the week.
     if double_time:
         double_time_hours = hours_in_day - STANDARD_HOURS_PER_DAY
-        #day_double_time_hours_left = DOUBLE_TIME_HOURS_PER_DAY - hours_in_day
-
-    #day_hours = count_day_hours()
-
-
-    """
-
-    # Check weekly hours worked first as they will override daily hours worked.
-    if hours_in_week <= STANDARD_HOURS_PER_WK:
-
-
-
-    # account for weekly rates
-    if hours_in_week <= STANDARD_HOURS_PER_WK:
-        # account for daily rates
-        if hours_in_day <= STANDARD_HOURS_PER_DAY:
-            pay = calc_standard_day_rate()
-        elif STANDARD_HOURS_PER_DAY < hours_in_day <= OVERTIME_HOURS_PER_DAY:
-            pay = calc_standard_day_rate() + calc_overtime_day_rate()
-        elif OVERTIME_HOURS_PER_DAY < hours_in_day <= DOUBLE_TIME_HOURS_PER_DAY:
-            pay = calc_standard_day_rate() + calc_overtime_day_rate() + calc_double_time_day_rate()
-
-    # every hour is now overtime
-    elif STANDARD_HOURS_PER_WK < hours_in_week <= OVERTIME_HOURS_PER_WK:
-        #hours_in_week = hours_in_day
-        print('Weekly threshold! Calculating overtime...')
-        overtime_hours = hours_in_week - STANDARD_HOURS_PER_WK
-        hours = hours_in_day - overtime_hours
-        print('overtime_hours:', overtime_hours)
-
-        pay = (hours * STANDARD_RATE * pay_rate) + (overtime_hours * OVERTIME_RATE * pay_rate)
-        #pay += hours_in_day * OVERTIME_RATE * pay_rate
-
-    # every hour is now double time
-   
-    elif hours_in_week > OVERTIME_HOURS_PER_WK:
-        # pay = (hours_in_week - OVERTIME_HOURS_PER_WK) * DOUBLE_TIME_RATE * pay_rate
-        print("Weekly threshold! Calculating double time...")
-        pay = (hours_in_week - OVERTIME_HOURS_PER_WK) * OVERTIME_RATE * pay_rate
-        pay += hours_in_day * DOUBLE_TIME_RATE * pay_rate
-    """
-    
- 
-    #hours = {"standard_hours": standard_hours, "overtime_hours": overtime_hours, "double_time_hours": double_time_hours}
 
     # Calculate the pay for the day based on the hours worked.
     pay = calc_standard_pay() + calc_overtime_pay() + calc_double_time_pay()
